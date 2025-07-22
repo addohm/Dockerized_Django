f48fb5e89485473e1e04af9a5236d0bde73be43b
Adam <addohm@users.noreply.github.com>
2025-07-22 13:33:33 +0800

    Update reset_prod_project.sh

--- reset_prod_project.sh
+++ reset_prod_project.sh
@@ -3,5 +3,5 @@
 docker compose down
 sudo rm -Rf ./staticfiles/*
 sudo rm -Rf ./mediafiles/*
-docker build --no-cache
+docker build -t django:latet . --no-cache
 docker-compose --file=docker-compose.yaml up --detach
