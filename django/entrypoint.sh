4590b06799b2ac953c07166730b6affc09dd859c
Adam <addohm@users.noreply.github.com>
2025-07-31 15:47:21 +0800

    Update entrypoint.sh

--- django/entrypoint.sh
+++ django/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-# chown -R django:django /project
-# exec runuser -u django "$@"
+
+# Fix permissions on mounted volumes
+chown -R django:django /project/staticfiles
+chown -R django:django /project/mediafiles
+chmod -R 775 /project/staticfiles /project/mediafiles
+
+# Run Django commands
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py createcachetable
@@ -8,10 +13,7 @@ python manage.py collectstatic --noinput
 
 if [ "$DJANGO_SUPERUSER_USERNAME" ]
 then
-    python manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL
+    python manage.py createsuperuser --noinput
 fi
 
-python -m gunicorn ${PROJECT_NAME}.wsgi:application --workers 3 --bind 0.0.0.0:${APP_PORT} 
+exec python -m gunicorn ${PROJECT_NAME}.wsgi:application --workers 3 --bind 0.0.0.0:${APP_PORT}
